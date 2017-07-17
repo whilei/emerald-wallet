9b25d021c900b586c4fb19f9bce95e526bf4b9f9
ia <isaac.ardis@gmail.com>
2017-07-17 14:56:27 -0500

    :floppy_disk:

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -2,10 +2,9 @@
 
 mkdir deploy
 
-for f in $(ls dist/ | grep -e "dmg" -e "zip" -e "deb" -e "tar.gz" -e "exe" -e "msi");
-do
-  mv dist/$f deploy/
-done
+# Globbing allows non-alphanumeric filenames (according to bash linter).
+mv ./dist/*.dmg ./dist/*.zip ./dist/*.deb ./dist/*.tar.get ./dist/*.exe ./dist/*.msi ./deploy/
 
 # Will use $GCP_PASSWD to decrypt encrypted json key
-janus deploy -to builds.etcdevteam.com/emerald-wallet/preview -files deploy/* -key gcloud-travis.json.enc
+# janus deploy -to="builds.etcdevteam.com/emerald-wallet/$(janus version -format='v%M.%m.x')/" -files="./deploy/*" -key="./gcloud-travis.json.enc"
+janus deploy -to="fake-builds-etcdevteam-com/emerald-wallet/$(janus version -format='v%M.%m.x')/" -files="./deploy/*" -key="./gcloud-travis.json.enc"
