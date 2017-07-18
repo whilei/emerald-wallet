6a6804ce059d4dc1babf0d60bd51ea5a2da8ff43
ia <isaac.ardis@gmail.com>
2017-07-18 07:17:54 -0500

    actually use isaac key

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -3,10 +3,10 @@
 mkdir deploy
 
 # Note: dont move .exe, AppVeyor will take care of that.
-for f in $(ls dist/ | grep -e "dmg" -e "zip" -e "deb" -e "tar.gz" -e "msi");
+for f in $(ls dist/ | grep -e "dmg" -e "zip" -e "deb" -e "tar.gz" -e "exe" -e "msi");
 do
   mv dist/$f deploy/
 done
 
 # Will use $GCP_PASSWD to decrypt encrypted json key
-janus deploy -to="builds.etcdevteam.com/emerald-wallet/$(janus version -format='v%M.%m.x')" -files="./deploy/*" -key="./gcloud-travis.json.enc"
+janus deploy -to="fake-builds-etcdevteam-com/emerald-wallet/$(janus version -format='v%M.%m.x')" -files="./deploy/*" -key="./isaac-test-gcp-service-key.enc.json"
